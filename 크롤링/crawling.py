5890ddc737ddc78a551bb5d05439a5066a7d4af3
chkim429 <ckdgjs000@naver.com>
2022-06-21 20:30:45 +0900

    crawling

--- 크롤링/crawling.py
+++ 크롤링/crawling.py
@@ -11,6 +11,7 @@ browser.find_element_by_xpath('//*[@id="NM_FAVORITE"]/div[1]/ul[1]/li[5]/a').cli
 
 search = browser.find_element_by_xpath('//*[@id="_verticalGnbModule"]/div/div[2]/div/div[2]/div/div[2]/form/fieldset/div/input')
 search.click()
+time
 search.send_keys('애플워치7')
 search.send_keys(Keys.ENTER)
 
@@ -24,3 +25,13 @@ while True:
     if new_height == last_height:
         break
     last_height = new_height
+    
+    
+mearchs = browser.find_elements_by_css_selector(".basicList_info_area__17Xyo")
+
+for mearch in mearchs:
+    name = mearch.find_element_by_css_selector(".basicList_title__3P9Q7").text
+    price = mearch.find_element_by_css_selector(".price_num__2WUXn").text
+    link = mearch.find_element_by_css_selector(".basicList_title__3P9Q7 > a").get_attribute('href')
+    
+    print(name,price,link)
