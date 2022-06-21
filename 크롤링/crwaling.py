7b98ff053f94e4273429f81a7e564c656e393109
chkim429 <ckdgjs000@naver.com>
2022-06-21 19:05:02 +0900

    search commit

--- 크롤링/crwaling.py
+++ 크롤링/crwaling.py
@@ -1,4 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome('C:/chromedriver.exe')
+browser.get('https://www.naver.com')
 
+
+browser.find_element_by_xpath('//*[@id="NM_FAVORITE"]/div[1]/ul[1]/li[5]/a').click()
+
+
+search = browser.find_element_by_xpath('//*[@id="_verticalGnbModule"]/div/div[2]/div/div[2]/div/div[2]/form/fieldset/div/input')
+search.click()
+search.send_keys('애플워치7')
+search.send_keys(Keys.ENTER)
